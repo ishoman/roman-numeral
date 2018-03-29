0a2949d3cd82bde27c13fe9a9a7bf27263177851
isaac shoman and julia sheremet <julia@>
2018-03-29 09:55:08 -0700

    Add logic for second spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 // Business Logic
+// var onesArray = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+// var tensArray = ['X', 'XX', 'X-XX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+// var hundredsArray = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+// var thousandsArray = ['M', 'MM', 'MMM'];
+
+    function romanNumeral(input) {
+      if (input === 1) {
+        return "I"
+      } else if (input === 2) {
+        return "II"
+      } else {
+        return "I don't know how to do that. My bad..."
+      }
+
+    }
 
 
 
@@ -10,9 +25,11 @@
 // JQuery User Interface Logic
 $(document).ready(function() {
   $("form#roman-numeral").submit(function(event) {
-    var input = parseInt($("#input").val());
-    $("#result").text(input);
     event.preventDefault();
+    var input = parseInt($("#input").val());
+    var romanNumeral1 = romanNumeral(input);
+    $("#result").text(romanNumeral1);
+
   });
 
 });
