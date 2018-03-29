38d355edbecf627768173795ff4011236d44d0ac
isaac shoman and julia sheremet <isaac@>
2018-03-29 10:42:53 -0700

    add backend logic to convert any single roman numeral

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,30 @@
       return "II";
     } else if (input === 3) {
       return "III";
+    } else if (input === 5) {
+      return "V";
     }  else if (input === 10) {
       return "X";
     } else if (input === 20) {
       return "XX";
     } else if (input === 30) {
       return "XXX";
+    } else if (input === 50) {
+      return "L";
+    }  else if (input === 100) {
+      return "C";
+    } else if (input === 200) {
+      return "CC";
+    } else if (input === 300) {
+      return "CCC";
+    } else if (input === 500) {
+      return "D";
+    }  else if (input === 1000) {
+      return "M";
+    } else if (input === 2000) {
+      return "MM";
+    } else if (input === 3000) {
+      return "MMM";
     } else {
       return "I don't know how to do that - my bad..."
     }
@@ -27,13 +45,60 @@
 // }
 
 //Helper Functions
-// var returnsI = function(ones) {
-//   if (ones === 1 || ones === 2 || ones === 3) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+var returnsI = function(ones) {
+  if (ones === 1 || ones === 2 || ones === 3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var returnsV = function(fives) {
+  if (fives === 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var returnsX = function(tens) {
+  if (tens === 10 || tens === 20 || tens === 30) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var returnsL = function(fifties) {
+  if (fifties === 50) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var returnsC = function(hundreds) {
+  if (hundreds === 100 || hundreds === 200 || hundreds === 300) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var returnsD = function(fiveHundred) {
+  if (fiveHundred === 500) {
+    return true;
+  } else {
+    return false;
+  }
+}
+var returnsM = function(thousands) {
+  if (thousands === 1000 || thousands === 2000 || thousands === 3000) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 
